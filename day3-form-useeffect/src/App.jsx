85099c2a4b40b2a6cbcd4628e7d2b41d5fcdd328
Negiparas26 <parasnegi10101@gmail.com>
2025-02-27 13:27:49 +0530

    day 3 commit

--- day3-form-useeffect/src/App.jsx
+++ day3-form-useeffect/src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [date, setDate] = useState(new Date().toLocaleDateString());
 
+  useEffect(() => {
+    // Create an interval
+    const interval = setInterval(() => {
+      setDate(new Date().toUTCString());
+    }, 1000);
+                                                                                           // console.log("count");
+                                                                                           // console.log(count);
+    // console.log(date);                                                                           |
+    // Cleanup function to clear interval when component unmounts                                   |
+    return () => clearInterval(interval);                                       //                  |
+   
+  }, []);  
+  useEffect(()=>{
+    const option =setInterval(()=>{
+      setCount((prev)=>prev+1);
+  
+     },1000);
+     return ()=>clearInterval(option); 
+  },[])
+ 
+ console.log(count);
   return (
     <>
       <div>
+        <h1>Date: {date}</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +52,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
