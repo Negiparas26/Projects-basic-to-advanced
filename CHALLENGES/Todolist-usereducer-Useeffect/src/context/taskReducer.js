cd40232e676dee37dfe2fe949af7880d592b7ff1
Negiparas26 <parasnegi10101@gmail.com>
2025-03-10 16:03:15 +0530

    .

--- CHALLENGES/Todolist-usereducer-Useeffect/src/context/taskReducer.js
+++ CHALLENGES/Todolist-usereducer-Useeffect/src/context/taskReducer.js
@@ -1,25 +1,3 @@
-// export const taskReducer = (state, action) => {
-//     switch (action.type) {
-//       case "ADD_TASK":
-//         return [...state, { id: Date.now(), text: action.payload, completed: false }];
-      
-//       case "TOGGLE_TASK":
-//         return state.map(task =>
-//           task.id === action.payload ? { ...task, completed: !task.completed } : task
-//         );
-  
-//       case "DELETE_TASK":
-//         return state.filter(task => task.id !== action.payload);
-  
-//       case "EDIT_TASK":
-//         return state.map(task =>
-//           task.id === action.payload.id ? { ...task, text: action.payload.newText } : task
-//         );
-  
-//       default:
-//         return state;
-//     }
-//   };
 export const initialState = JSON.parse(localStorage.getItem("tasks")) || [];
 
 export const taskReducer = (state, action) => {
